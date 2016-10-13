163141c1d6ee0e89fe6e79cfb99a99cfa5a653a3
Ian Dennis Miller <ian@iandennismiller.com>
2016-10-13 13:39:46 -0400

    apply theme, labels to all plots

--- analysis/plots.R
+++ analysis/plots.R
@@ -34,8 +34,10 @@ per_capita_metro_plot =
 
 # trends_plot
 
-trends_plot = ggplot(data=merged) +
-  geom_line(aes(color=city, x=year_offset, y=fatalities_per_capita, group=city)) +
+trends_plot = 
+  ggplot(data=merged, aes(x=year_offset, y=fatalities_per_capita, group=city)) +
+  geom_line(aes(color=city), size=0.1, show.legend = FALSE) +
+  geom_point(aes(color=city), size=0.1, show.legend = FALSE) +
   geom_abline(aes(linetype=city,color=city, slope=slope, intercept=intercept)) +
   ylim(0, 0.8) +
   scale_x_continuous(
@@ -43,23 +45,59 @@ trends_plot = ggplot(data=merged) +
     labels=as.character(predicted_years),
     limits=c(0, 15)
   ) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    fte_theme() +
+    geom_hline(yintercept=0, size=0.2, color="black") +
+    theme(legend.position="top") +
+    guides(col=guide_legend(nrow=1, label.position = "bottom")) +
+    theme(legend.key.width=unit(1, "cm")) +
+    labs(
+      title="Pedestrian and Cyclist deaths per capita, with future trend line",
+      x="Year",
+      y="# fatalities per 100,000 residents"
+    )
 
 # nyc_toronto_plot
 
 d2 = d[ which(d$city %in% c("Toronto", "New York City")), ]
-nyc_toronto_plot = ggplot(data=d2, aes(x=year, y=fatalities_per_capita, group=city)) +
-  geom_line(aes(color=city), show.legend = FALSE) +
+nyc_toronto_plot = 
+  ggplot(data=d2, aes(x=year, y=fatalities_per_capita, group=city)) +
+  # geom_line(aes(color=city), show.legend = FALSE) +
+  geom_line(aes(color=city, linetype=city), size=0.2, show.legend = FALSE) +
+  geom_point(aes(color=city), size=0.1, show.legend = FALSE) +
   geom_smooth(method = "lm", aes(linetype=city, group=city, color=city)) +
-  scale_x_continuous(breaks = years)
+  scale_x_continuous(breaks = years) +
+  fte_theme() +
+  geom_hline(yintercept=0, size=0.2, color="black") +
+  theme(legend.position="top") +
+  guides(col=guide_legend(nrow=1, label.position = "bottom")) +
+  theme(legend.key.width=unit(1, "cm")) +
+  labs(
+    title="Pedestrian and Cyclist deaths per capita: Focus on Toronto and New York City",
+    x="Year",
+    y="# fatalities per 100,000 residents"
+  )
 
 # amsterdam_toronto_plot
 
-d2 = d[ which(d$city %in% c("Amsterdam", "Toronto")), ]
-amsterdam_toronto_plot = ggplot(data=d2, aes(x=year, y=fatalities_per_capita, group=city)) +
-  geom_line(aes(color=city), show.legend = FALSE) +
+d2 = d[ which(d$city %in% c("Toronto", "Amsterdam")), ]
+d2$city = as.factor(d2$city)
+amsterdam_toronto_plot = 
+  ggplot(data=d2, aes(x=year, y=fatalities_per_capita, group=city, order=as.numeric(city))) +
+  geom_line(aes(color=city, linetype=city), size=0.2, show.legend = FALSE) +
+  geom_point(aes(color=city), size=0.1, show.legend = FALSE) +
   geom_smooth(method = "lm", aes(linetype=city, group=city, color=city)) +
-  scale_x_continuous(breaks = years)
+  scale_x_continuous(breaks = years) +
+  fte_theme() +
+  geom_hline(yintercept=0, size=0.2, color="black") +
+  theme(legend.position="top") +
+  guides(col=guide_legend(nrow=1, label.position = "bottom")) +
+  theme(legend.key.width=unit(1, "cm")) +
+  labs(
+    title="Pedestrian and Cyclist deaths per capita: Focus on Toronto and Amsterdam",
+    x="Year",
+    y="# fatalities per 100,000 residents"
+  )
 
 # trend_table
 
